a6794d04b86c0b9a9a34850460452038773cb4e1
Mihai Luta <luta.mihai.92@gmail.com>
2025-01-07 20:48:55 +0200

    Remove test.py

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 from dreams_mc.make_model_card import generate_modelcard
+from plotConfidence import plot_confidence_intervals
+import numpy as np
 
-config_file_path = './config.yaml'
-output_path = './logs/model_card.html'
-version_num = '2.0'
-generate_modelcard(config_file_path, output_path, version_num)
+#config_file_path = './config.yaml'
+#output_path = './logs/model_card.html'
+#version_num = '2.0'
+#generate_modelcard(config_file_path, output_path, version_num)
+
+predictions = np.array([
+    [0.70, 0.20, 0.10],
+    [0.15, 0.75, 0.10], 
+    [0.05, 0.10, 0.85], 
+    [0.40, 0.35, 0.25], 
+    [0.60, 0.25, 0.15]
+])
+plot_confidence_intervals(predictions, save_dir='./logs')
